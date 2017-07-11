077e342e0fb2ff10ba1cbe15eddbacea1339f5d3
cafischer <coralinefischer@gmail.com>
2017-07-11 09:29:36 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bac_project',
+      version='0.1',
+      description='Simulation of single neurons and neuronal networks',
+      url='https://github.com/cafischer/bac_project',
+      author='Caroline Fischer',
+      author_email='coralinefischer@gmail.com',
+      license='',
+      packages=['bac_project'],
+      zip_safe=False)
